233d94f901d87f2c38d1fb8882187e57ab6f0a2a
Mern Stack <mernstack.js@gmail.com>
2025-10-15 14:43:11 +0200

    Add ask

---
+++ node.js_essential_training/Ch01/ask.js
@@ -0,0 +1,23 @@
+// ask.js - simple Node.js readline example
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("What is your name? ", (name) => {
+  console.log(`Hello, ${name}!`);
+
+  rl.question("How old are you? ", (age) => {
+    console.log(`You are ${age} years old.`);
+
+    rl.close();
+  });
+});
+
+rl.on("close", () => {
+  console.log("Goodbye!");
+  process.exit(0);
+});
