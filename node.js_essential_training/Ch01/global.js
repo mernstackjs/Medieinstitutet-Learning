8e2ee2a4bb768f5371bec3e0d3ea6076141b3444
Mern Stack <mernstack.js@gmail.com>
2025-10-15 13:57:34 +0200

    Add global.js

---
+++ node.js_essential_training/Ch01/global.js
@@ -0,0 +1,19 @@
+// node.js have a global objects and variables you can use without importing them
+
+// Common Node.js Globals
+
+// 1. __dirname : The full path of the folder where the current file is located
+// 2. __filename: The full path of the current file
+// 3. global: The global object (similar to window in browsers)
+// 4. console: Print messages to the terminal (like console.log)
+// 5. process: Provides info about the Node.js process and environment
+// 6. setTimeout: Run a function after a certain time
+// 7. setInterval: Run a function repeatedly at intervals
+
+// let hello = "Hello World from Node.js";
+// let justNode = hello.slice(17);
+// console.log(`Who let the ${justNode} out?`);
+// const path = require("path");
+// console.log(`The file name is ${path.basename(__filename)}`);
+
+// console.log(process.argv);
