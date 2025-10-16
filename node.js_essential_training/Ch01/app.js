522655d30cfc432ee928597145a4ea6acbb29e93
Mern Stack <mernstack.js@gmail.com>
2025-10-16 13:38:41 +0200

    Add module and exports

---
+++ node.js_essential_training/Ch01/app.js
@@ -0,0 +1,8 @@
+const myModule = require("./myModule");
+
+console.log(myModule.inc());
+console.log(myModule.inc());
+console.log(myModule.dec());
+console.log(myModule.getCount());
+console.log(myModule.who);
+console.log(myModule.anything);
